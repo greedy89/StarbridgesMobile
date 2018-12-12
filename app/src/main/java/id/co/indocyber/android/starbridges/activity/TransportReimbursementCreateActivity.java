e08ae0fb46ba41faf11f5971a9425fca62948d8f
Technocenter Lead <lead.technocenter@acc.co.id>
2018-12-12 13:39:36 +0700

    add year month picker dialog

--- app/src/main/java/id/co/indocyber/android/starbridges/activity/TransportReimbursementCreateActivity.java
+++ app/src/main/java/id/co/indocyber/android/starbridges/activity/TransportReimbursementCreateActivity.java
@@ -1,16 +1,33 @@
 package id.co.indocyber.android.starbridges.activity;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.twinkle94.monthyearpicker.picker.YearMonthPickerDialog;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import id.co.indocyber.android.starbridges.R;
 import id.co.indocyber.android.starbridges.utility.MonthYearPickerDialog;
+import id.co.indocyber.android.starbridges.utility.YearMonthPickerDialogCustom;
 
 public class TransportReimbursementCreateActivity extends AppCompatActivity {
 
@@ -23,19 +40,64 @@ public class TransportReimbursementCreateActivity extends AppCompatActivity {
 
         txtPeriodCreateTR = (EditText)findViewById(R.id.txtPeriodCreateTR);
 
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2018,10,01);
+
+
+        final YearMonthPickerDialogCustom yearMonthPickerDialog = new YearMonthPickerDialogCustom(this, new YearMonthPickerDialog.OnDateSetListener() {
+            @Override
+            public void onYearMonthSet(int year, int month) {
+                Calendar calendar=Calendar.getInstance();
+                calendar.add(Calendar.MONTH,-2);
+                calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), 01);
+                calendar.set(Calendar.HOUR,0);
+                calendar.set(Calendar.HOUR_OF_DAY, 0);
+                calendar.set(Calendar.MINUTE, 0);
+                calendar.set(Calendar.SECOND, 0);
+                calendar.set(Calendar.MILLISECOND, 0);
+
+                Calendar selectedCalendar=Calendar.getInstance();
+                selectedCalendar.set(year,month, 01);
+                selectedCalendar.set(Calendar.HOUR,0);
+                selectedCalendar.set(Calendar.HOUR_OF_DAY, 0);
+                selectedCalendar.set(Calendar.MINUTE, 0);
+                selectedCalendar.set(Calendar.SECOND, 0);
+                selectedCalendar.set(Calendar.MILLISECOND, 1);
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy");
+                if(selectedCalendar.after(calendar))
+                    txtPeriodCreateTR.setText(dateFormat.format(selectedCalendar.getTime()));
+
+            }
+        });
+
+
+
         txtPeriodCreateTR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MonthYearPickerDialog pickerDialog = new MonthYearPickerDialog();
-                pickerDialog.setListener(new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int year, int month, int i2) {
-                        Toast.makeText(TransportReimbursementCreateActivity.this, year + "-" + month, Toast.LENGTH_SHORT).show();
-                    }
-                });
-                pickerDialog.show(getSupportFragmentManager(), "MonthYearPickerDialog");
+                yearMonthPickerDialog.show();
+
             }
         });
+
+    }
+
+
+    private void showCustomDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.dialog_month_year_picker);
+        dialog.setCancelable(true);
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
     }
 
     private DatePickerDialog createDialogWithoutDateField() {
